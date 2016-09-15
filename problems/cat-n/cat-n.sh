dbda1b5d9c8b4af31eeee08e758d383ddb096709
Kyle James Bachelor <kbache@service2.ice.americas.sgi.com>
2016-09-15 11:50:37 -0700

    solution

--- problems/cat-n/cat-n.sh
+++ problems/cat-n/cat-n.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "OK"
+set -u
+
+if [[ $# -lt 1 ]]; then
+	echo "Usage: cat-n.sh file"
+	exit 1
+fi
+
+INPUTFILECAT=$1
+LINECOUNTCATA=$(wc -l $INPUTFILECAT)
+LINECOUNTCATB=$(echo $LINECOUNTCATA | tr -cd '[[:digit:]]')
+let LINECOUNTCATB++
+i=1
+
+while [[ $i -lt $LINECOUNTCATB ]]; do
+	CURRENTLINEOFTEXT=$(sed "${i}q;d" $INPUTFILECAT)
+	printf "%g %s\n" $i $CURRENTLINEOFTEXT
+	let i++
+done
